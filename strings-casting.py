94344092ca7817b5e118d7c35f3303035276d80b
Your Name <you@example.com>
2021-03-23 11:25:36 +0000

    Data types and operators

---
+++ strings-casting.py
@@ -0,0 +1,15 @@
+# Strings and Casting
+greetings = 'Hello World!'
+single_quotes = 'single quotes \'WOW\''
+double_quotes = "double quotes 'WOW'"
+print(single_quotes)
+print(double_quotes)
+
+# Indexing starts from 0
+# H e l l o   W o r l d  !
+# 0 1 2 3 4 5 6 7 8 9 10 11
+
+# String slicing
+print(greetings[0:5]) # Prints first 5 characters in greetings
+print(greetings[6:11]) # Prints characters 5-11
+print(greetings[-1]) # Prints the final character in greetings
